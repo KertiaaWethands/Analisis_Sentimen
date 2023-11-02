fed2558da4badbc66afa94b7475907744d34013a
Robby Sobari <112602385+KertiaaWethands@users.noreply.github.com>
2023-11-02 15:29:18 +0700

    last
    last

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 df = pd.read_csv('dataset.csv')  # Ganti 'dataset.csv' dengan nama file dataset Anda
 
 
-# Route untuk halaman sentimen
 @app.route('/')
 def sentimen():
     # Menghitung jumlah data dengan label positif, negatif, dan netral
@@ -19,15 +18,12 @@ def sentimen():
     total_data = len(df)
     
     # Menghitung persentase
-    positif_percentage = (positif_count / total_data) * 100
-    negatif_percentage = (negatif_count / total_data) * 100
-    netral_percentage = (netral_count / total_data) * 100
+    positif_percentage = "{:.2f}%".format((positif_count / total_data) * 100)
+    negatif_percentage = "{:.2f}%".format((negatif_count / total_data) * 100)
+    netral_percentage = "{:.2f}%".format((netral_count / total_data) * 100)
     
     return render_template('sentimen.html', 
                            positif=positif_percentage, negatif=negatif_percentage, netral=netral_percentage)
-
-from flask import jsonify
-
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query')
@@ -39,31 +35,28 @@ def search():
         negatif_count = len(filtered_df[filtered_df['Label'] == 'Negatif'])
         netral_count = len(filtered_df[filtered_df['Label'] == 'Netral'])
 
-        # Menghitung persentase sentimen
+        # Menghitung total sentimen
         total_sentimen = positif_count + negatif_count + netral_count
-        positif_percentage = (positif_count / total_sentimen) * 100
-        negatif_percentage = (negatif_count / total_sentimen) * 100
-        netral_percentage = (netral_count / total_sentimen) * 100
+
+        if total_sentimen == 0:
+            positif_percentage = "0.00%"  # Atur nilai default ke nol jika total_sentimen adalah nol
+            negatif_percentage = "0.00%"
+            netral_percentage = "0.00%"
+        else:
+            # Menghitung persentase sentimen
+            positif_percentage = "{:.2f}%".format((positif_count / total_sentimen) * 100)
+            negatif_percentage = "{:.2f}%".format((negatif_count / total_sentimen) * 100)
+            netral_percentage = "{:.2f}%".format((netral_count / total_sentimen) * 100)
 
         # Mengambil hasil pencarian dalam bentuk dictionary
         search_results = filtered_df.to_dict(orient='records')
 
-        # return jsonify({
-        #     'search_results': search_results,
-        #     'positif': positif_percentage,
-        #     'negatif': negatif_percentage,
-        #     'netral': netral_percentage,
-        #     'query': query
-        # })
         return render_template('sentimen.html', 
                            positif=positif_percentage, negatif=negatif_percentage, netral=netral_percentage, search_results=search_results)
     else:
         return jsonify({
             'error': 'Kata kunci pencarian tidak ditemukan'
         })
-    
-    
-
 
 if __name__ == '__main__':
     app.run(debug=True)
